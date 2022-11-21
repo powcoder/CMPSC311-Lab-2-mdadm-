54bc81ae36066d64202950b356364116f9732fc6
powcoder <powcoder@163.com>
2022-11-21 10:21:05 +0800

    CMPSC311 Lab 2 mdadm

---
+++ mdadm.c
@@ -0,0 +1,25 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+//CMPSC 311 SP22
+//LAB 2
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "mdadm.h"
+#include "jbod.h"
+
+int mdadm_mount(void) {
+  return -1;
+}
+
+int mdadm_unmount(void) {
+  return -1;
+}
+
+int mdadm_read(uint32_t addr, uint32_t len, uint8_t *buf) {
+  return len;
+}
---
+++ tester.h
@@ -0,0 +1,17 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+//CMPSC 311 SP22
+//LAB 2
+#ifndef TEST_H_
+#define TEST_H_
+
+#include "jbod.h"
+
+int jbod_sign_block(int disk_num, int block_num);
+void jbod_initialize_drives_contents();
+
+#define MAX_IO_SIZE 1024
+
+#endif
